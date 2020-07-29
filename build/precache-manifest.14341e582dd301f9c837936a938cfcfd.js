837450cfbb05ccbd620bf46e9dd0b3bb0c46967e
Alex <15ambm@queensu.ca>
2020-07-29 11:36:33 -0400

    added prod build files

---
+++ build/precache-manifest.14341e582dd301f9c837936a938cfcfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a516d1311572819cd2329dff39ac7589",
+    "url": "/index.html"
+  },
+  {
+    "revision": "779b484666cdc65d116e",
+    "url": "/static/css/main.cad1bc8f.chunk.css"
+  },
+  {
+    "revision": "bfa73816b98f665d7884",
+    "url": "/static/js/2.af12328e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.af12328e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779b484666cdc65d116e",
+    "url": "/static/js/main.8b63df04.chunk.js"
+  },
+  {
+    "revision": "6aec218a32ca873f1723",
+    "url": "/static/js/runtime-main.904694b2.js"
+  }
+]);
